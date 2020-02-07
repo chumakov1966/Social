7db0891a7b25020b39031fafdbfa0e475acd5aa3
Вячеслав Чумаков <57487174+chumakov1966@users.noreply.github.com>
2020-02-07 15:38:26 +0400

    Create SocialUser.php
    Создан контроллер

---
+++ app/Http/Controllers/SocialUser.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Http\Controllers;
+use App\Models\Social as SocialUser;
+use App\Models\User as AllUser
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+class SocialUser extends Controller{
+	
+}
+?>
