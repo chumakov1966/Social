3c74679c5a5209e5c63d350a8e41db471027343d
Вячеслав Чумаков <57487174+chumakov1966@users.noreply.github.com>
2020-02-07 17:19:00 +0400

    Update SocialUser.php
    Разбираюсь с формой

--- app/Http/Controllers/SocialUser.php
+++ app/Http/Controllers/SocialUser.php
@@ -1,10 +1,22 @@
 <?php
 namespace App\Http\Controllers;
+
 use App\Models\Social as SocialUser;
-use App\Models\User as AllUser
+use App\Models\User as AllUser;
+use App\Http\Controllers\Auth;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\AuthForgotPasswordController;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\ResetPasswordController;
+use App\Http\Controllers\Auth\VerificationController;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+
 class SocialUser extends Controller{
-	
+	function CheckIn(AllUser $users){
+		$user=$users->User();
+		return view ('auth.login'
+		
+	}
 }
 ?>
